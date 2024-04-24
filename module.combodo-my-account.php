3d50a95d469d94de8faf51f99e075a48a3b3d55a
Eric Espie <eric.espie@combodo.com>
2024-04-24 17:39:49 +0200

    iTop 3.2.0 dependency

--- module.combodo-my-account.php
+++ module.combodo-my-account.php
@@ -5,7 +5,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'combodo-my-account/1.0.0',
+	'combodo-my-account/3.2.0',
 	array(
 		// Identification
 		//
@@ -15,7 +15,7 @@ SetupWebPage::AddModule(
 		// Setup
 		//
 		'dependencies' => array(
-			
+			'itop-welcome-itil/3.2.0||itop-structure/3.2.0',
 		),
 		'mandatory' => false,
 		'visible' => true,
