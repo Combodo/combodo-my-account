9b941cf45826cb77c5911c3f55b4d82fd9545d77
Benjamin Dalsass <benjamin.dalsass@combodo.com>
2025-08-18 11:39:43 +0200

    N°8609 - Remove utils::GetClassesForInterface

--- index.php
+++ index.php
@@ -7,12 +7,13 @@
 use Combodo\iTop\MyAccount\Controller\MyAccountController;
 use Combodo\iTop\MyAccount\Helper\MyAccountHelper;
 use Combodo\iTop\MyAccount\Hook\iMyAccountTabContentExtension;
+use Combodo\iTop\Service\InterfaceDiscovery\InterfaceDiscovery;
 
 require_once(APPROOT.'application/startup.inc.php');
 
 // Get the additional paths
 $aAdditionalPaths = [];
-foreach (utils::GetClassesForInterface(iMyAccountTabContentExtension::class, '', ['[\\\\/]lib[\\\\/]', '[\\\\/]node_modules[\\\\/]', '[\\\\/]test[\\\\/]', '[\\\\/]tests[\\\\/]']) as $sExtensionClass) {
+foreach (InterfaceDiscovery::GetInstance()->FindItopClasses(iMyAccountTabContentExtension::class) as $sExtensionClass) {
 	/** @var \Combodo\iTop\MyAccount\Hook\iMyAccountTabContentExtension $oExtension */
 	$oExtension = new $sExtensionClass();
 	$aAdditionalPaths[] = $oExtension->GetTemplatePath();
